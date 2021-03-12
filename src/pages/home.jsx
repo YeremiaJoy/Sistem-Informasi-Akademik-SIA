66098ed7c32d94ef4222cf0b7ceb085a0fa6f84c
sep150900 <79404091+sep150900@users.noreply.github.com>
2021-03-12 13:49:26 +0700

    create, update folder

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                <p>Haloo</p>
+            </div>
+        )
+    }
+}
+export default home;

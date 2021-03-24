00d2c1fd9ef0e30147e2b85ed8921e00f368afce
YeremiaJoy <yeremiajoy@gmail.com>
2021-03-24 15:29:20 +0700

    login button

--- front-end/src/component/Navbar/NavbarComponent.jsx
+++ front-end/src/component/Navbar/NavbarComponent.jsx
@@ -1,46 +1,33 @@
-import React, { Component } from "react"
+import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 import { Link } from 'react-router-dom'
 import { NavbarData } from '../Navbar/NavbarComponentData'
+import "./NavbarComponent.css"
 
 const NavbarComponent = () => {
-    const style={
-        activeStyle: {
-            color: '#ffffff',
-            textDecoration: "none",
-            paddingRight: '5px',
-            paddingLeft: '5px'
-        },
-        navitem: {
-        color: '#737373',
-        fontWeight: 'bold',
-        textDecoration: "none",
-        paddingRight: '5px',
-        paddingLeft: '5px'
-    }
-    };
-        
- 
-    return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/">Sistem Informasi Akademik</Navbar.Brand>
-            <Nav>
-                {NavbarData.map((val, key) => {
-                    return (
-                        <Link
-                            key={key}
-                            to={val.link} style={window.location.pathname == val.link ? style.activeStyle : style.navitem}
-                            onClick={() => { window.location.pathname = val.link; }}
-                        >
-                            <div>{val.name}</div>
-                        </Link>
-                    )
-                }
+
+return (
+    <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Sistem Informasi Akademik</Navbar.Brand>
+        <Nav>
+            {NavbarData.map((val, key) => {
+                return (
+                    <Link
+                        key={key} style={{textDecoration: "none"}}
+                        to={val.link} className={window.location.pathname == val.link ? "activeStyle" : "navitem"}
+                        onClick={() => { window.location.pathname = val.link; }}
+                    >
+                        <div>{val.name}</div>
+                    </Link>
                 )
-                }
+            }
+            )
+            }
+            <div className="batas"></div>
+            <Link to="/login" className="login">Login</Link>
             </Nav>
-        </Navbar>
-    );
+    </Navbar>
+);
 }
 
 export default NavbarComponent;

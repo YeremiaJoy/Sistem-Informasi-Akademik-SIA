40e1304215c7860e37103154d3b4821afd22e54e
YeremiaJoy <yeremiajoy@gmail.com>
2021-03-24 16:11:23 +0700

    login button

--- front-end/src/component/Navbar/NavbarComponent.jsx
+++ front-end/src/component/Navbar/NavbarComponent.jsx
@@ -1,33 +1,49 @@
-import React from "react"
+import React, { Component } from "react"
 import { Navbar, Nav } from "react-bootstrap"
 import { Link } from 'react-router-dom'
 import { NavbarData } from '../Navbar/NavbarComponentData'
 import "./NavbarComponent.css"
+import Login from "../Login/Login"
 
-const NavbarComponent = () => {
-
-return (
-    <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Sistem Informasi Akademik</Navbar.Brand>
-        <Nav>
-            {NavbarData.map((val, key) => {
-                return (
-                    <Link
-                        key={key} style={{textDecoration: "none"}}
+class NavbarComponent extends Component {
+   constructor(props) {
+       super(props);
+       this.state = {
+          LoginShow: false
+       }
+   }
+   render() {
+      let loginClose = () => this.setState({ LoginShow: false });
+      return (
+         <Navbar bg="dark" variant="dark" >
+            <Navbar.Brand href="/">Sistem Informasi Akademik</Navbar.Brand>
+            <Nav>
+               {NavbarData.map((val, key) => {
+                  return (
+                     <Link
+                        key={key} style={{ textDecoration: "none" }}
                         to={val.link} className={window.location.pathname == val.link ? "activeStyle" : "navitem"}
                         onClick={() => { window.location.pathname = val.link; }}
-                    >
+                     >
                         <div>{val.name}</div>
-                    </Link>
-                )
-            }
-            )
-            }
-            <div className="batas"></div>
-            <Link to="/login" className="login">Login</Link>
+                     </Link>
+                  )
+               }
+               )
+               }
+               <div className="batas"></div>
+               <Link className="login" onClick={() => this.setState({
+                  LoginShow: true
+               })}>Login</Link>
+               <Login 
+               show={this.state.LoginShow}
+               onHide={loginClose}
+               />
+               <Link to="/register" className="register">Register</Link>
             </Nav>
-    </Navbar>
-);
+         </Navbar>
+      );
+   }
 }
 
 export default NavbarComponent;

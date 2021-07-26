1edd37ddfbf8afdbd95988d22178d9c091e9248f
YeremiaJoy <yeremiajoy@gmail.com>
2021-07-26 23:02:40 +0700

    Update home.jsx

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -6,14 +6,6 @@ import "./home.css";
 
 class home extends Component {
   render() {
-    // const style = {
-    //   tab: {
-    //     marginTop: "2%",
-    //   },
-    //   container: {
-    //     padding: "0px",
-    //   },
-    // };
     return (
       <>
         <NavbarComponent />
@@ -39,32 +31,8 @@ class home extends Component {
               Login as Teacher
             </Button>
           </div>
-        </div>
-        {/* <Container fluid style={style.container}>
-
-          <Tabs defaultActiveKey="Major" id="uncontrolled-tab-example" style={style.tab}>
-            <Tab eventKey="Major" title="Major">
-              <Card.Body href="#major">
-                <Card.Title >Major </Card.Title>
-                <Card.Text>
-                  Press the button to show any Major.
-              </Card.Text>
-                <Button href="/major" variant="primary">Show Major ..</Button>
-              </Card.Body>
-            </Tab>
-
-            <Tab eventKey="Student" title="Student">
-              <Card.Body>
-                <Card.Title >Student </Card.Title>
-                <Card.Text>
-                  Press the button to show any Students.
-              </Card.Text>
-                <Button href="/student" variant="primary">Show Students ..</Button>
-              </Card.Body>
-            </Tab>
-
-          </Tabs>
-        </Container> */}
+          <div className="footer-login" style={{color: "white"}}>© 2021, Sistem Informasi Akademik | create by Michael Septian & Yeremia Joy </div>
+        </div>        
       </>
     );
   }
